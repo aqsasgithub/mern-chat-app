2374b6e905e06534ac67346df48ce8f5a273789f
Aqsa Syed <aqsas2774@gmail.com>
2025-07-24 20:47:38 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://aqsa:AqsaS2774.@cluster0.d030bn7.mongodb.net/ChatDB');
+        await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
         console.log('MongoDB connected successfully✅');
     } catch (error) {
         console.log(error.message);
